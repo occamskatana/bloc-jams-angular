3fb4ddf26c92cfff4aacb578f7c8407c7187e45c
occamskatana <johngallweycarter@gmail.com>
2016-04-07 19:17:09 -0400

    checkpoint and assignment completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -20,20 +20,25 @@
 		}
 
 
+		var playSong = function(song){
+			currentBuzzObject.play();
+
+			song.playing = true
+		}
+
+
 		SongPlayer.play = function(song) {
 
 			if (currentSong !== song){
 
 					setSong(song);
-					currentBuzzObject.play();
-					song.playing = true;
+					playSong(song);
 
 			} else if (currentSong === song) {
 
 				if (currentBuzzObject.isPaused()) {
 
-					currentBuzzObject.play();
-					song.playing = true;
+				playSong(song);
 
 				}
 			}
