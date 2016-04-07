f667a4bd118e2998f91f9f734628113826f3b001
occamskatana <johngallweycarter@gmail.com>
2016-04-07 16:19:59 -0400

    bugfix

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
 (function() {
-	function AlbumCtrl(Fixtures){
-		this.albumData = Fixtures.getAlbum;
-		this.songs = albumPicasso.songs;
-		
+	function AlbumCtrl(Fixtures, SongPlayer){
+		this.albumData = Fixtures.getAlbum();
+		console.log(this.albumData)
+		this.songs = this.albumData.songs;
+		this.songPlayer = SongPlayer;
 	}
 
 	angular
 		.module('blocJams')
-		.controller('AlbumCtrl',['Fixtures', AlbumCtrl]);
+		.controller('AlbumCtrl',['Fixtures', 'SongPlayer', AlbumCtrl]);
 
 })();
