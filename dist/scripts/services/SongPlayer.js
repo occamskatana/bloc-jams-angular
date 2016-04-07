5fe5fc4fede9c9975b8dd2f01401f8be69b143dd
occamskatana <johngallweycarter@gmail.com>
2016-04-07 16:57:45 -0400

    checkpoint 7 complete

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,16 +1,52 @@
 (function(){
 	function SongPlayer() {
 		var SongPlayer = {};
+		var currentSong = null;
+		var currentBuzzObject = null;
+
+		var setSong = function(song){
+			if (currentBuzzObject){
+					currentBuzzObject.stop();
+					currentSong.playing = null;
+				}
+
+
+				currentBuzzObject = new buzz.sound(song.audioUrl, {
+					formats: ['mp3'],
+					preload: true
+				});
+
+				currentSong = song
+		}
 
 
 		SongPlayer.play = function(song) {
-			var currentBuzzObject = new buzz.sound(song.audioUrl, {
-				formats: ['mp3'],
-				preload: true
-			});
 
-			currentBuzzObject.play();
+			if (currentSong !== song){
+
+					setSong(song);
+					currentBuzzObject.play();
+					song.playing = true;
+
+			} else if (currentSong === song) {
+
+				if (currentBuzzObject.isPaused()) {
+
+					currentBuzzObject.play();
+					song.playing = true;
+
+				}
+			}
 		};
+
+
+		SongPlayer.pause = function(song) {
+			currentBuzzObject.pause();
+			song.playing = false;
+		};
+
+
+
 
 		return SongPlayer;
 	}
